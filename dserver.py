72e4875f727b12b3275c3ad1e91e062f2e14e7c8
DishaAgarwal03 <97750861+DishaAgarwal03@users.noreply.github.com>
2023-10-13 19:11:43 +0530

    Added authentication key

--- dserver.py
+++ dserver.py
@@ -5,16 +5,13 @@
 # add passing of authentication key
 # add concurrency
 # handle keyboard interrupt 
-
+# add key generation and exchange in client_aes_try and server_aes_try
+import uuid
 import socket
-# hostname = socket.gethostname()
-# IPAddr = socket.gethostbyname(hostname)
-# print("Your Computer Name is:" + hostname)
-# print("Server IP Address is:" + IPAddr)
 
 userdict={ "user1":"one", "user2":"two" , "user3":"three" }
-IPAddr="10.52.1.147"
-port = 10201
+IPAddr="10.0.2.15"
+port = 10200
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
     s.bind((IPAddr,port))
     s.listen(5)
@@ -31,21 +28,20 @@ with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
                     conn.sendall(b"Enter your password")
                     data = conn.recv(256)
                     pwd = str(data, 'UTF-8')
-                    # print("Recieved Passwd")
                     if userdict[username]==pwd:
-                        # print(f"sending Passwd: {pwd}")
                         conn.sendall(b"You are connected")
-                        # print("Passwd sent")
+                        unique_id=uuid.uuid4(32)
+                        auth_key=str(unique_id).encode()
+                        # add authentication key to the database
+                        conn.sendall(auth_key)
                         conn.close() 
-                        exit(0)
                     else:
-                        conn.sendall(b"Wrong Password Enter password again?(Y/N): ")
+                        conn.sendall(b"Wrong Password")
+                        conn.sendall(b"Enter password again?(Y/N): ")
                         choice = conn.recv(256)
                         choice = str(choice, 'UTF-8')
                         if(choice=="N" or choice=="n"):
-                            conn.sendall(b"Closing Connection")
                             conn.close()
-                            exit(0)
                         elif(choice=="Y" or choice=="y"):
                             continue 
                         else:
@@ -56,9 +52,7 @@ with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
                 choice = conn.recv(256)
                 choice = str(choice, 'UTF-8')
                 if(choice=="C" or choice=="c"):
-                    conn.sendall(b"Closing Connection")
                     conn.close()
-                    exit(0)
                 elif(choice=="B" or choice=="b"):
                     continue
                 elif(choice=="A" or choice=="a"):
@@ -73,18 +67,17 @@ with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
                         reg_pwd = conn.recv(256)
                         reg_pwd = str(reg_pwd, 'UTF-8')
                         userdict[reg_username]=reg_pwd
-                        conn.sendall(b"Successfully Registered! Login?(Y/N):")
+                        conn.sendall(b"Successfully Registered!")
+                        conn.sendall(b"Login?(Y/N): ")
                         choice = conn.recv(256)
                         choice = str(choice, 'UTF-8')
                         if(choice=="N" or choice=="n"):
-                            conn.sendall(b"Closing Connection")
                             conn.close()
-                            exit(0)
                         elif(choice=="Y" or choice=="y"):
                             break
                         else:
                             conn.sendall(b"Wrong choice.") 
-                            break  
+                            conn.close()  
                 else:
                     conn.sendall(b"Wrong choice.")
                     continue
